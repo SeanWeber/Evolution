ca50dc1a415a88faa3b363deacb3a1b613bdc852
SeanWeber <bluesmew@gmail.com>
2012-08-07 02:00:36 -0400

    changed structure to make program more managable

--- evo.js
+++ evo.js
@@ -13,10 +13,16 @@
   along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-var width = 500;
-var height = 600;
 var cells = [];
 var food = [];
+
+experiment={
+  width:500,
+  height:600,
+  startingCells: 50,
+  foodRate: .05,
+  mutationRate: 1,
+}
 
 function cell(x,y,color,direction,speed,vision,energy,maxSize){
   this.x = x;
@@ -37,16 +43,16 @@ function foodParticle(X, Y){
 function init(){
   var c = document.getElementById('game');
   var game = c.getContext('2d'); 
-  c.width = width;
-  c.height = height;
+  c.width = experiment.width;
+  c.height = experiment.height;
   game.fillStyle = '#000';
-  game.fillRect(0, 0, width, 600);
-
-  var pColor = 1;
+  game.fillRect(0, 0, experiment.width, 600);
+  var pColor = 0;
+  
   //populates the first generation of cells  
-  for(i=0;i<50;i++){
-    var x = Math.floor(Math.random() * width);
-    var y = Math.floor(Math.random() * height);
+  for(i=0;i<experiment.startingCells;i++){
+    var x = Math.floor(Math.random() * experiment.width);
+    var y = Math.floor(Math.random() * experiment.height);
     var color = 'hsl('+pColor+', 90%, 50%)';
     var direction = Math.floor((Math.random()*8));
     var speed = Math.random() * 2;
@@ -54,7 +60,7 @@ function init(){
     var energy = Math.floor((Math.random()*1000)) + 3500; //3500 to 4500
     var maxSize = Math.floor((Math.random()*1000)) + 4500; //4500 to 5500
 
-    pColor +=7;
+    pColor += (360 / experiment.startingCells);
     cells.push(new cell(x,y,color,direction,speed,vision,energy,maxSize));  
   }
   report();
@@ -69,9 +75,9 @@ function live(){
 }
 
 function feed(){ 
-  if(Math.random() > .95){
-    var X = Math.floor(Math.random() * width);
-    var Y = Math.floor(Math.random() * height);
+  if(Math.random() < experiment.foodRate){
+    var X = Math.floor(Math.random() * experiment.width);
+    var Y = Math.floor(Math.random() * experiment.height);
     food.push(new foodParticle(X, Y));
   }
 }
@@ -82,7 +88,7 @@ function draw(){
 
   //clears the previous frame
   game.fillStyle = '#000';
-  game.fillRect(0, 0, width, 600);
+  game.fillRect(0, 0, experiment.width, 600);
 
   //draws each cell
   for(i=0;i<cells.length;i++){
@@ -150,7 +156,7 @@ function move(){
     }
     
     //Cells lose energy each turn proportional to their speed
-    cells[i].energy -= cells[i].speed;
+    cells[i].energy -= (cells[i].speed +1);
     
     //Reproduce if they have enough energy 
     if(cells[i].energy > cells[i].maxSize){
@@ -182,13 +188,13 @@ function wander(){
     }
     
     //Makes sure cell stays in bounds
-    if(cells[i].x > width){
+    if(cells[i].x > experiment.width){
       cells[i].direction = 0;
     }
     if(cells[i].x < 0){
       cells[i].direction = 4;
     }
-    if(cells[i].y > height){
+    if(cells[i].y > experiment.height){
       cells[i].direction = 6;
     }
     if(cells[i].y < 0){
@@ -234,11 +240,11 @@ function mitosis(parent){
   var x = cells[parent].x;
   var y = cells[parent].y;
   var color = cells[parent].color;
-  var direction = Math.floor((Math.random()*8));
-  var speed = cells[parent].speed + (.5 - Math.random()); //mutate +/-1
-  var vision = cells[parent].vision + (2 - Math.floor((Math.random()*5))); //+/-2
   var energy = cells[parent].energy;
-  var maxSize = cells[parent].maxSize + (50 - Math.floor((Math.random()*100)));
+  var direction = Math.floor((Math.random()*8));
+  var speed = cells[parent].speed + (.5 - Math.random())*experiment.mutationRate;
+  var vision=cells[parent].vision+(2-Math.floor((Math.random()*5)))*experiment.mutationRate;
+  var maxSize=cells[parent].maxSize+(50-Math.floor((Math.random()*100)))*experiment.mutationRate;
   cells.push(new cell(x,y,color,direction,speed,vision,energy,maxSize));
   checkDNA(i);
   report();
@@ -254,18 +260,15 @@ function report(){
     avgVision += cells[i].vision;
     avgMaxSize += cells[i].maxSize;
   }
-  
   avgSpeed /= cells.length;
   avgVision /= cells.length;
   avgMaxSize /= cells.length * 1000;
-  
-  var experimentReport = "<table>"+
-    "<tr><td>Number of cells:</td><td>" + cells.length + "</td></tr>" +
-    "<tr><td>Average Speed:</td><td>" + avgSpeed.toFixed(3) + "</td></tr>" +
-    "<tr><td>Average Vision:</td><td>" + avgVision.toFixed(3) + "</td></tr>" +
-    "<tr><td>Average Max Size:</td><td>" + avgMaxSize.toFixed(3)+"</td></tr></table>";
-               
-  document.getElementById("report").innerHTML= experimentReport;
+
+  //writes data to table               
+  document.getElementById("amount").innerHTML = cells.length;
+  document.getElementById("avgSpeed").innerHTML = avgSpeed.toFixed(3);
+  document.getElementById("avgVision").innerHTML = avgVision.toFixed(3);
+  document.getElementById("avgMaxSize").innerHTML = avgMaxSize.toFixed(3);
 }
 
 function checkDNA(i){
@@ -279,19 +282,18 @@ function cellInfo(event){
   var y=event.clientY -16; //...
   var selected = 0;
   
+  //checks if the mouse clicked near a cell
   for(var i=0; i<cells.length;i++){
     if((x < cells[i].x+10)&&(x > cells[i].x-10)&&(y < cells[i].y+10)&&(y > cells[i].y-10)){
       selected = cells[i];
     }
   }
-  if(selected!=0){
-    cellReport = "<table>" +
-      "<tr><td>Color:</td><td id='colorTab'> </td></tr>" +
-      "<tr><td>Speed:</td><td>" + selected.speed.toFixed(3) + "</td></tr>" +
-      "<tr><td>Vision:</td><td>" + selected.vision + "</td></tr>" +
-      "<tr><td>Max Size:</td><td>"+(selected.maxSize/1000).toFixed(3)+"</td></tr></table>";
-      
-    document.getElementById("cellInfo").innerHTML= cellReport;
+  
+  //writes data to table if a cell was found
+  if(selected!=0){    
     document.getElementById("colorTab").style.backgroundColor= selected.color;
+    document.getElementById("speed").innerHTML = selected.speed.toFixed(3);
+    document.getElementById("vision").innerHTML = selected.vision;
+    document.getElementById("maxSize").innerHTML = (selected.maxSize/1000).toFixed(3);
   }
 }
